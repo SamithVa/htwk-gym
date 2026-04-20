69244bb862f62f733d3144efcdd834304c1f964c
SamithVA <vasamith@163.com>
2026-04-20 20:38:18 +0800

    add import for KickingRobust environment in __init__.py

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,4 +1,5 @@
 from envs.T1.base_walk import BaseWalk
 from envs.T1.kicking import Kicking
+from envs.T1.kicking_robust import KickingRobust
 from envs.K1.parameter_walk import ParameterWalk
 from envs.T1.parameter_walk import ParameterWalk
